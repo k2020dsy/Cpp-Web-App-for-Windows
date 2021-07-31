88c07a41566c216d4cd5a61895f66943862af617
k2020dsy <88242815+k2020dsy@users.noreply.github.com>
2021-08-01 01:17:24 +0800

    Add files via upload

---
+++ stdafx.h
@@ -0,0 +1,9 @@
+#pragma once
+#pragma warning(disable:4996)
+#include <iostream>
+#include <codecvt>
+#include "Base64.h"
+#include "crulBuild/include/curl/curl.h"
+#include "json/json.h"
+using namespace std;
+#pragma comment(lib,"libcurl.lib")
