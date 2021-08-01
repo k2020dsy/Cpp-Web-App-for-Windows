aa9f9710ffd7c927055d34c73615e0c933b4a42a
k2020dsy <88242815+k2020dsy@users.noreply.github.com>
2021-08-01 10:58:10 +0800

    Add files via upload

---
+++ base64decoder.py
@@ -0,0 +1,31 @@
+import os,base64,sys
+
+#start from here
+
+str1 = ''
+Saveas = sys.argv[1]
+# 打开文件
+Base64FileCache = open("base64cache.cache", "r")
+
+for line in Base64FileCache.readlines():                          #依次读取每行  
+    line = line.strip()  #去掉每行头尾空白  
+    str1 = str1 + line;
+    print ("读取的数据为: %s" % (line))
+ 
+# 关闭文件
+Base64FileCache.close()
+
+if len(Saveas) :# 把字符串还原成图片
+    with open(Saveas,'wb') as str2image:
+        str2image.write(base64.b64decode(str1))
+        print("base64 decode finish||Python IDE")
+        os.system("pause");
+else:
+    print("File reading ERROR,Cache is empty")
+
+    
+
+
+
+
+
